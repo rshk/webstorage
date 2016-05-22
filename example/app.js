5739ebbf292af240add3afd61ecd0dfe1a2f1e47
Samuele Santi <redshadow@hackzine.org>
2016-05-22 22:47:32 +0100

    Simple usage example app

---
+++ example/app.js
@@ -0,0 +1,60 @@
+$(function() {
+
+    var SERVER_URL = 'http://127.0.0.1:5000';
+    var $textarea = $('#text'),
+        $form = $('#form');
+
+    function flash_message(selector, text) {
+        $(selector).text(text).show();
+        setTimeout(function() {$(selector).fadeOut()}, 1000);
+    }
+
+    function show_saved_message(hash) {
+        flash_message('#msg-saved', 'Saved as ' + hash);
+    }
+
+    function show_error(text) {
+        flash_message('#msg-error', text);
+    }
+
+    function load_from_hash(hash) {
+        if (!hash || hash == '#') {
+            return;
+        }
+        hash = hash.slice(1);
+        $textarea.val('Loading...');
+        console.log('Loading data for ' + hash);
+        $.get(SERVER_URL + '/' + hash)
+            .done(function(data) {
+                console.log('Got data: ' + data);
+                $textarea.val(data);
+            })
+            .fail(function(xhr) {
+                show_error('Error loading data: ' + xhr.status);
+                $textarea.val('');
+                window.location.hash = '';
+            });
+    }
+
+    window.onhashchange = function() {
+        console.log('Hash: ' + window.location.hash);
+        load_from_hash(window.location.hash);
+    };
+    load_from_hash(window.location.hash);
+
+    $('#form').submit(function(evt) {
+        evt.preventDefault();
+
+        var text = $textarea.val();
+        $.ajax(SERVER_URL,
+               {data: text, method: 'POST', contentType: 'text/plain'})
+
+            .done(function(data) {
+                window.location.hash = data;
+            })
+            .fail(function(xhr) {
+                show_error('Failed saving data: ' + xhr.status);
+            });
+    });
+
+});
