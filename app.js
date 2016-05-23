053db972258ada52f457980c22af3293f0768fdd
Samuele Santi <redshadow@hackzine.org>
2016-05-23 10:15:16 +0000

    Update documentation

--- app.js
+++ app.js
@@ -2,19 +2,14 @@ $(function() {
 
     var SERVER_URL = 'https://webstorage.herokuapp.com';
     var $textarea = $('#text'),
-        $form = $('#form');
+        $form = $('#form'),
+        $save_button = $('#save-button'),
+        $messages = $('#messages');
 
-    function flash_message(selector, text) {
-        $(selector).text(text).show();
-        setTimeout(function() {$(selector).fadeOut()}, 1000);
-    }
-
-    function show_saved_message(hash) {
-        flash_message('#msg-saved', 'Saved as ' + hash);
-    }
-
-    function show_error(text) {
-        flash_message('#msg-error', text);
+    function flash_message(level, text) {
+        var $el = $('<div>').attr('class', 'alert alert-' + level).html(text);
+        $messages.append($el);
+        setTimeout(function() { $el.fadeOut(); }, 2000);
     }
 
     function load_from_hash(hash) {
@@ -29,11 +24,60 @@ $(function() {
                 console.log('Got data: ' + data);
                 $textarea.val(data);
             })
-            .fail(function(xhr) {
-                show_error('Error loading data: ' + xhr.status);
+            .fail(function(xhr, msg, error) {
+                flash_message('danger', 'Error loading data: ' + xhr.status +
+                              ' ' + error);
                 $textarea.val('');
                 window.location.hash = '';
             });
+    }
+
+    function save_text(text) {
+        save_start();
+        return $.ajax(SERVER_URL,
+               {data: text, method: 'POST', contentType: 'text/plain'})
+            .done(function(data) {
+                save_end(true);
+                window.location.hash = data;
+                flash_message('success', 'Your text was saved as ' + data);
+            })
+            .fail(function(xhr, msg, error) {
+                save_end(false);
+                flash_message(
+                    'danger', 'Error while attempting to save text: ' +
+                        xhr.status + ' ' + error);
+            });
+    }
+
+    function save_start() {
+        $save_button
+            .attr('disabled', true)
+            .removeClass('btn-primary')
+            .removeClass('btn-success')
+            .removeClass('btn-danger')
+            .addClass('btn-warning');
+        $save_button.children('span').attr('class', 'fa fa-spinner fa-spin');
+    }
+
+    function save_end(success) {
+        $save_button
+            .attr('disabled', false)
+            .removeClass('btn-warning');
+        if (success) {
+            $save_button.children('span').attr('class', 'fa fa-check');
+            $save_button.addClass('btn-success');
+        }
+        else {
+            $save_button.children('span').attr('class', 'fa fa-times');
+            $save_button.addClass('btn-danger');
+        }
+        setTimeout(function() {
+            $save_button.children('span').attr('class', 'fa fa-cloud');
+            $save_button
+                .removeClass('btn-success')
+                .removeClass('btn-danger')
+                .addClass('btn-primary');
+        }, 1000);
     }
 
     window.onhashchange = function() {
@@ -42,19 +86,17 @@ $(function() {
     };
     load_from_hash(window.location.hash);
 
-    $('#form').submit(function(evt) {
+    $form.submit(function(evt) {
         evt.preventDefault();
-
         var text = $textarea.val();
-        $.ajax(SERVER_URL,
-               {data: text, method: 'POST', contentType: 'text/plain'})
+        save_text(text);
+    });
 
-            .done(function(data) {
-                window.location.hash = data;
-            })
-            .fail(function(xhr) {
-                show_error('Failed saving data: ' + xhr.status);
-            });
+    $textarea.keydown(function(e) {
+        if (e.ctrlKey && e.keyCode == 13) {
+            // Ctrl-Enter pressed
+            $form.submit();
+        }
     });
 
 });
